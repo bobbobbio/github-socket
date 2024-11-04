54d2b1da209b86192946e21aec77e794c7e0cdb0
Remi Bernotavicius <remi@abort.cc>
2024-11-04 15:36:41 -0800

    decode backend ids

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+#[derive(Debug)]
+struct BackendIds {
+    workflow_run_backend_id: String,
+    workflow_job_run_backend_id: String,
+}
+
+fn decode_backend_ids(token: &str) -> BackendIds {
+    use base64::Engine as _;
+
+    let mut token_parts = token.split(".").skip(1);
+    let decoded = base64::engine::general_purpose::STANDARD
+        .decode(token_parts.next().unwrap())
+        .unwrap();
+    let v = serde_json::from_slice::<serde_json::Value>(&decoded).unwrap();
+
+    let mut scp_parts = v
+        .get("scp")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .split(" ")
+        .map(|p| p.split(":").collect::<Vec<_>>())
+        .filter(|p| p[0] != "Actions.Results");
+    let scope_parts = scp_parts.next().unwrap();
+
+    BackendIds {
+        workflow_run_backend_id: scope_parts[1].into(),
+        workflow_job_run_backend_id: scope_parts[2].into(),
+    }
+}
+
 struct Client {
     client: reqwest::Client,
     owner: String,
@@ -5,6 +36,7 @@ struct Client {
     run_id: String,
     token: String,
     base_url: String,
+    backend_ids: BackendIds,
 }
 
 impl Client {
@@ -16,36 +48,41 @@ impl Client {
         let owner = parts.next().unwrap().into();
         let repo = parts.next().unwrap().into();
         let run_id = std::env::var("GITHUB_RUN_ID").unwrap();
-        let token = std::env::var("ACTIONS_RUNTIME_TOKEN").unwrap();
+        let token = std::env::var("GH_TOKEN").unwrap();
+        let runtime_token = std::env::var("ACTIONS_RUNTIME_TOKEN").unwrap();
 
-        use base64::Engine as _;
-        let mut token_parts = token.split(".").skip(1);
-        let token_decoded = base64::engine::general_purpose::STANDARD.decode(token_parts.next().unwrap()).unwrap();
-        let v = serde_json::from_slice::<serde_json::Value>(&token_decoded).unwrap();
-        println!("{v:#?}");
+        let backend_ids = decode_backend_ids(&runtime_token);
+        println!("{backend_ids:#?}");
 
         let base_url = "https://api.github.com".into();
 
         Self {
-            client, owner, repo, run_id, token, base_url
+            client,
+            owner,
+            repo,
+            run_id,
+            token,
+            base_url,
+            backend_ids,
         }
     }
 
     async fn get(&self, path: &str) {
-        let resp = self.client
+        let resp = self
+            .client
             .get(format!(
                 "{base_url}/repos/{owner}/{repo}/actions/runs/{run_id}/artifacts{path}",
-                base_url=&self.base_url,
-                owner=&self.owner,
-                repo=&self.repo,
-                run_id=&self.run_id,
+                base_url = &self.base_url,
+                owner = &self.owner,
+                repo = &self.repo,
+                run_id = &self.run_id,
             ))
-            .header(
-                "Accept",
-                "application/vnd.github.v3+json",
-            )
+            .header("Accept", "application/vnd.github.v3+json")
             .header("User-Agent", "@actions/artifact-2.1.11")
-            .header("Authorization", &format!("Bearer {token}", token=&self.token))
+            .header(
+                "Authorization",
+                &format!("Bearer {token}", token = &self.token),
+            )
             .send()
             .await
             .unwrap();
