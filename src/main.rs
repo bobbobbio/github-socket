b682c43166bbe0c7c1d3cbfdc7920c423aa63f53
Remi Bernotavicius <remi@abort.cc>
2024-11-05 17:15:41 -0800

    start progress on socket abstraction

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use anyhow::{bail, Result};
+use azure_storage_blobs::prelude::BlobClient;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -100,7 +101,7 @@ struct FinalizeArtifactRequest {
     #[serde(flatten)]
     backend_ids: BackendIds,
     name: String,
-    size: u32,
+    size: usize,
 }
 
 #[derive(Serialize)]
@@ -149,7 +150,7 @@ impl GhClient {
         }
     }
 
-    async fn upload(&self, name: &str, content: &str) {
+    async fn start_upload(&self, name: &str) -> BlobClient {
         let req = CreateArtifactRequest {
             backend_ids: self.client.backend_ids.clone(),
             name: name.into(),
@@ -167,18 +168,14 @@ impl GhClient {
 
         let upload_url =
             url::Url::parse(resp.get("signed_upload_url").unwrap().as_str().unwrap()).unwrap();
-        let blob_client =
-            azure_storage_blobs::prelude::BlobClient::from_sas_url(&upload_url).unwrap();
-        blob_client
-            .put_block_blob(content.to_owned())
-            .content_type("text/plain")
-            .await
-            .unwrap();
+        BlobClient::from_sas_url(&upload_url).unwrap()
+    }
 
+    async fn finish_upload(&self, name: &str, content_length: usize) {
         let req = FinalizeArtifactRequest {
             backend_ids: self.client.backend_ids.clone(),
             name: name.into(),
-            size: content.len() as u32,
+            size: content_length,
         };
         self.client
             .request::<_, serde_json::Value>(
@@ -188,6 +185,16 @@ impl GhClient {
             )
             .await
             .unwrap();
+    }
+
+    async fn upload(&self, name: &str, content: &str) {
+        let blob_client = self.start_upload(name).await;
+        blob_client
+            .put_block_blob(content.to_owned())
+            .content_type("text/plain")
+            .await
+            .unwrap();
+        self.finish_upload(name, content.len()).await;
     }
 
     async fn list(&self) -> Vec<Artifact> {
@@ -206,7 +213,7 @@ impl GhClient {
         resp.artifacts
     }
 
-    async fn download(&self, backend_ids: BackendIds, name: &str) -> String {
+    async fn start_download(&self, backend_ids: BackendIds, name: &str) -> BlobClient {
         let req = GetSignedArtifactUrlRequest {
             backend_ids,
             name: name.into(),
@@ -221,9 +228,128 @@ impl GhClient {
             .await
             .unwrap();
         let url = url::Url::parse(resp.get("signed_url").unwrap().as_str().unwrap()).unwrap();
-        let blob_client = azure_storage_blobs::prelude::BlobClient::from_sas_url(&url).unwrap();
+        BlobClient::from_sas_url(&url).unwrap()
+    }
+
+    async fn download(&self, backend_ids: BackendIds, name: &str) -> String {
+        let blob_client = self.start_download(backend_ids, name).await;
         let content = blob_client.get_content().await.unwrap();
         String::from_utf8_lossy(&content[..]).into()
+    }
+}
+
+use azure_storage_blobs::blob::operations::GetBlobResponse;
+use bytes::Bytes;
+use futures_core::Stream;
+use std::future::Future;
+use std::io;
+use std::pin::{pin, Pin};
+use std::sync::Arc;
+use std::task::{ready, Context, Poll};
+use tokio::io::{AsyncRead, AsyncReadExt as _, ReadBuf};
+use tokio_util::io::StreamReader;
+
+struct BlobBytesStream {
+    response_body_stream: azure_core::Pageable<GetBlobResponse, azure_core::Error>,
+    response_body: Option<azure_core::ResponseBody>,
+}
+
+impl BlobBytesStream {
+    fn new(client: BlobClient) -> Self {
+        Self {
+            response_body_stream: client.get().into_stream(),
+            response_body: None,
+        }
+    }
+}
+
+impl Stream for BlobBytesStream {
+    type Item = io::Result<Bytes>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        if let Some(body) = &mut self.response_body {
+            if let Some(value) = ready!(futures_util::Stream::poll_next(pin!(body), cx)) {
+                return Poll::Ready(Some(
+                    value.map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string())),
+                ));
+            } else {
+                self.response_body = None;
+            }
+        }
+        if let Some(resp) = ready!(futures_util::Stream::poll_next(
+            pin!(&mut self.response_body_stream),
+            cx
+        )) {
+            let resp = resp.map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+            self.response_body = Some(resp.data);
+            self.poll_next(cx)
+        } else {
+            Poll::Ready(None)
+        }
+    }
+}
+
+enum GhReadSocketState {
+    Reading(StreamReader<BlobBytesStream, Bytes>),
+    Getting(Pin<Box<dyn Future<Output = Result<BlobClient>> + Send + 'static>>),
+}
+
+struct GhReadSocket {
+    client: Arc<GhClient>,
+    remote_backend_ids: BackendIds,
+    unique_id: String,
+    sequence_id: u64,
+    state: GhReadSocketState,
+}
+
+impl GhReadSocket {
+    fn new(remote_backend_ids: BackendIds, unique_id: String) -> Self {
+        let client = Arc::new(GhClient::new());
+        Self {
+            client: client.clone(),
+            remote_backend_ids: remote_backend_ids.clone(),
+            unique_id: unique_id.clone(),
+            sequence_id: 2,
+            state: GhReadSocketState::Getting(Box::pin(async move {
+                Ok(client
+                    .start_download(remote_backend_ids, &format!("{unique_id}-1"))
+                    .await)
+            })),
+        }
+    }
+}
+
+impl AsyncRead for GhReadSocket {
+    fn poll_read(
+        mut self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<io::Result<()>> {
+        match &mut self.state {
+            GhReadSocketState::Reading(r) => {
+                ready!(pin!(r).poll_read(cx, buf))?;
+                if buf.filled().is_empty() {
+                    let client = self.client.clone();
+                    let remote_backend_ids = self.remote_backend_ids.clone();
+                    let next = format!("{}-{}", self.unique_id, self.sequence_id);
+                    self.state = GhReadSocketState::Getting(Box::pin(async move {
+                        Ok(client.start_download(remote_backend_ids, &next).await)
+                    }));
+                    self.sequence_id += 1;
+                    self.poll_read(cx, buf)
+                } else {
+                    Poll::Ready(Ok(()))
+                }
+            }
+            GhReadSocketState::Getting(f) => {
+                let blob_client = ready!(pin!(f).poll(cx))
+                    .map_err(|e| io::Error::new(io::ErrorKind::Other, e.to_string()))?;
+                self.state = GhReadSocketState::Reading(StreamReader::new(BlobBytesStream::new(
+                    blob_client,
+                )));
+                self.poll_read(cx, buf)
+            }
+        }
     }
 }
 
@@ -244,21 +370,26 @@ async fn main() {
 
     if std::env::var("ACTION").unwrap() == "1" {
         println!("sending ping");
-        client.upload("foo1", "ping").await;
+        client.upload("foo-1", "ping").await;
         println!("sent ping");
 
-        let backend_ids = wait_for_artifact(&client, "foo2").await;
+        let backend_ids = wait_for_artifact(&client, "foo-2").await;
 
-        let content = client.download(backend_ids, "foo2").await;
+        let content = client.download(backend_ids, "foo-2").await;
         println!("received message {content:?}");
     } else {
-        let backend_ids = wait_for_artifact(&client, "foo1").await;
+        let backend_ids = wait_for_artifact(&client, "foo-1").await;
 
-        let content = client.download(backend_ids, "foo1").await;
+        //let content = client.download(backend_ids, "foo-1").await;
+
+        let socket = GhReadSocket::new(backend_ids, "foo".into());
+        let mut content = String::new();
+        socket.take(4).read_to_string(&mut content).await.unwrap();
+
         println!("received message {content:?}");
 
         println!("sending pong");
-        client.upload("foo2", "pong").await;
+        client.upload("foo-2", "pong").await;
         println!("sent pong");
     }
 }
